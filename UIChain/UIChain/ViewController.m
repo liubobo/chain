a0e3a08a5371f4a4421b64ec7f0fe558a7eecea7
liubobo <602318557@qq.com>
2018-06-12 17:02:23 +0800

    提交注释

--- UIChain/UIChain/ViewController.m
+++ UIChain/UIChain/ViewController.m
@@ -7,20 +7,20 @@
 //
 #import "KMUIMakerHeader.h"
 #import "ViewController.h"
-#define kUCFont(size) [UIFont systemFontOfSize:(size)]
-#define kFont21 kUCFont(21)
-#define kFont20 kUCFont(20)
-#define kFont19 kUCFont(19)
-#define kFont18 kUCFont(18)
-#define kFont17 kUCFont(17)
-#define kFont16 kUCFont(16)
-#define kFont15 kUCFont(15)
-#define kFont14 kUCFont(14)
-#define kFont13 kUCFont(13)
-#define kFont12 kUCFont(12)
-#define kFont11 kUCFont(11)
-#define kFont10 kUCFont(10)
-#define kFont9 kUCFont(9)
+#define kAMFont(size) [UIFont systemFontOfSize:(size)]
+#define kFont21 kAMFont(21)
+#define kFont20 kAMFont(20)
+#define kFont19 kAMFont(19)
+#define kFont18 kAMFont(18)
+#define kFont17 kAMFont(17)
+#define kFont16 kAMFont(16)
+#define kFont15 kAMFont(15)
+#define kFont14 kAMFont(14)
+#define kFont13 kAMFont(13)
+#define kFont12 kAMFont(12)
+#define kFont11 kAMFont(11)
+#define kFont10 kAMFont(10)
+#define kFont9  kAMFont(9)
 
 @interface ViewController ()
 @property (nonatomic, strong) UIButton *b;
